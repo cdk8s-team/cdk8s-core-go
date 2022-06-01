b350c325b6b6a373ae53160bad63e7e8f3053df6
cdk8s-automation <cdk8s-team@amazon.com>
2022-06-01 00:24:34 +0000

    chore(release): v2.3.13

--- cdk8s/jsii/jsii.go
+++ cdk8s/jsii/jsii.go
@@ -12,7 +12,7 @@ import (
 	constructs "github.com/aws/constructs-go/constructs/v10/jsii"
 )
 
-//go:embed cdk8s-2.3.12.tgz
+//go:embed cdk8s-2.3.13.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -22,5 +22,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("cdk8s", "2.3.12", tarball)
+	_jsii_.Load("cdk8s", "2.3.13", tarball)
 }
