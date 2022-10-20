7172daf2f06c818e5e23e73e317946e90465295b
cdk8s-automation <cdk8s-team@amazon.com>
2022-10-20 00:14:15 +0000

    chore(release): v1.8.20

--- cdk8s/cdk8s_Lazy__runtime_type_checks.go
+++ cdk8s/cdk8s_Lazy__runtime_type_checks.go
@@ -1,5 +1,4 @@
 //go:build !no_runtime_type_checking
-// +build !no_runtime_type_checking
 
 // This is the core library of Cloud Development Kit (CDK) for Kubernetes (cdk8s). cdk8s apps synthesize into standard Kubernetes manifests which can be applied to any Kubernetes cluster.
 package cdk8s
--- cdk8s/go.mod
+++ cdk8s/go.mod
@@ -1,8 +1,8 @@
 module github.com/cdk8s-team/cdk8s-core-go/cdk8s
 
-go 1.16
+go 1.18
 
 require (
-	github.com/aws/jsii-runtime-go v1.69.0
-	github.com/aws/constructs-go/constructs/v3 v3.4.127
+	github.com/aws/jsii-runtime-go v1.70.0
+	github.com/aws/constructs-go/constructs/v3 v3.4.128
 )
