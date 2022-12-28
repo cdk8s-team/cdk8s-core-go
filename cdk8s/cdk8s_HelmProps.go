de6200e983384aebb160cc36cf16200efbdbde27
cdk8s-automation <cdk8s-team@amazon.com>
2022-12-28 14:01:15 +0000

    chore(release): v1.9.0

--- cdk8s/cdk8s_HelmProps.go
+++ cdk8s/cdk8s_HelmProps.go
@@ -16,11 +16,27 @@ type HelmProps struct {
 	HelmExecutable *string `field:"optional" json:"helmExecutable" yaml:"helmExecutable"`
 	// Additional flags to add to the `helm` execution.
 	HelmFlags *[]*string `field:"optional" json:"helmFlags" yaml:"helmFlags"`
+	// Scope all resources in to a given namespace.
+	Namespace *string `field:"optional" json:"namespace" yaml:"namespace"`
 	// The release name.
 	// See: https://helm.sh/docs/intro/using_helm/#three-big-concepts
 	//
 	ReleaseName *string `field:"optional" json:"releaseName" yaml:"releaseName"`
+	// Chart repository url where to locate the requested chart.
+	Repo *string `field:"optional" json:"repo" yaml:"repo"`
 	// Values to pass to the chart.
 	Values *map[string]interface{} `field:"optional" json:"values" yaml:"values"`
+	// Version constraint for the chart version to use.
+	//
+	// This constraint can be a specific tag (e.g. 1.1.1)
+	// or it may reference a valid range (e.g. ^2.0.0).
+	// If this is not specified, the latest version is used
+	//
+	// This name is passed to `helm template --version` and has all the relevant semantics.
+	//
+	// Example:
+	//   "^2.0.0"
+	//
+	Version *string `field:"optional" json:"version" yaml:"version"`
 }
 
